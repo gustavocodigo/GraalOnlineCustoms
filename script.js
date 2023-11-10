88da3ed7c6ae470f14e7403848cb5aa8bd67c81e
José Gustavo <108258194+gustavocodigo@users.noreply.github.com>
2023-11-09 21:54:17 -0300

    added download files with random name,
    not exposing ids

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ let close_alert = () => {}
                 return
             }
 
-            downloadFIle(url, null)
+            downloadFIle(url, btoa(Math.random()))
         }
         
 
